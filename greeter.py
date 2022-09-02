3aa11f9f831b4d37df93f8bc953e15d383613f9b
Dave Cardenas <khar3jee@pm.me>
2022-09-02 02:53:59 -0700

    Add time of day method

--- greeter.py
+++ greeter.py
@@ -3,10 +3,10 @@ Chapter 3
 Example Greeter for e-commerce sight
 
 Goal: write up Greeter class w/ 3x methods
-- _day
-    - gives current day
-- _part_of_day
-    - gives time of day (e.g. morning)
+x _day
+    x gives current day
+x _part_of_day
+    x gives time of day (e.g. morning)
 - greet(self, store)
     - prints message similar to:
     'Hi, welcome to <store>!
@@ -15,7 +15,7 @@ Goal: write up Greeter class w/ 3x methods
 - store will be attribute to name of store
 """
 
-from datetime import date as d
+from datetime import date as d, datetime as dt
 
 class Greeter():
     """example e-commerce greeter"""
@@ -23,13 +23,38 @@ class Greeter():
     def __init__(self):
         """greeter attributes, store name"""
         self.store_name = 'Super Awesome Store of Things'
+        self.coupon = '20%'
 
-    def main(self):
-        self.get_day()
+    def greet(self):
+        """
+        Calls _get_day, _time_of_day and 
+        prints greeting
+        """
+        day = self._get_day()
+        time = self._get_time_of_day()
+        print(f"Hi, welcome to {self.store_name}!\
+        \nHow's your {day} {time} going?\nHere's a coupon for {self.coupon} off")
 
-    def get_day(self):
+    def _get_day(self):
+        """
+        Gets date from datetime.date and
+        returns current day of the week (e.g. Monday)
+        """
         current_day = d.today()
-        print(d.strftime(current_day, "%A"))
+        return d.strftime(current_day, "%A")
+
+    def _get_time_of_day(self):
+        """
+        Gets time from some nether-hole of datetime
+        and returns time of day as string (e.g. morning)
+        """
+        current_time = dt.now()
+        if current_time.hour < 12:
+            return 'morning'
+        elif 12 <= current_time < 17:
+            return 'afternoon'
+        else:
+            return 'evening'
 
 greeter = Greeter()
-greeter.main()
+greeter.greet()
