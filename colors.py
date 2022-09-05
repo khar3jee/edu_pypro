8b8d87b6f3566aa0367c4a3cc77ede6984b21636
Dave Cardenas <khar3jee@pm.me>
2022-09-04 19:03:51 -0700

    Refactor colors into functions add find item in set function to colors add additional colors to colors test file

--- colors.py
+++ colors.py
@@ -1,32 +1,48 @@
 """
-change color counter to read line by line
-add color file for testing
+x change color counter to read line by line
+x add color file for testing
 Prof Johnson for the file path win (adjunct profs, best profs)
+new issue, check data to produce unique color set
 """
 
 import os
 import sys
+from typing import KeysView
 
 #sys.path[0] - returns the path the python file is running in.
 #os.path.join - concatenates the file with the file path
 
+
 color_counts = {}
-file_path = os.path.join(sys.path[0] + "\\" + "colors.txt")
+FILE_PATH = os.path.join(sys.path[0] + "\\" + "colors.txt")
 
-with open(file_path) as favorite_colors_file:
+def open_file():
+    """opens and reads files outputs a list"""
+    with open(FILE_PATH) as favorite_colors_file:
     #favorite_colors = favorite_colors_file.read().splitlines()
-    favorite_colors = []
-    for line in favorite_colors_file: #.readlines():
-        #print(line)
-        favorite_colors.append(line.strip())
-        #print(favorite_colors)
+        #favorite_colors = []
+        favorite_colors = set()
+        for line in favorite_colors_file: #.readlines():
+            #print(line)
+            favorite_colors.add(line.strip())
+            #print(favorite_colors)
+        return favorite_colors
+fav_colors = open_file()
 
-#print(favorite_colors)
-for color in favorite_colors:
-#    print(color)
-    if color in color_counts:
-        color_counts[color] += 1
-    else:
-        color_counts[color] = 1
+def color_sort(color_list):
+    """sort items based off a list and add to a dict"""
+    for color in color_list:
+        if color in color_counts:
+            color_counts[color] += 1
+        else:
+            color_counts[color] = 1
+    return color_counts
 
-print(color_counts)
+def check_list(sought_item, item_list):
+    return sought_item in item_list
+
+#print(set(color_sort(open_file()).keys()))
+# ^screwing around^
+#print('green' in color_counts)
+#print('green' in favorite_colors)
+print(check_list('green', fav_colors))
