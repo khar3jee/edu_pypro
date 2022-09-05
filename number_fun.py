399816bc5b1abe3f2d56d3897cd50d91d811da10
Dave Cardenas <khar3jee@pm.me>
2022-09-05 02:25:47 -0700

    Refactor functions in number_fun to use max and Counter from collections module

--- number_fun.py
+++ number_fun.py
@@ -2,23 +2,23 @@
 Numbers and counting
 Change the functions into more efficient ones
 (write up what I learned with functions from collections)
+https://docs.python.org/3/library/collections.html#collections.defaultdict
 """
 
+from collections import Counter
+
+num_list = [1, 2, 3, 4, 5, 6, 3, 7, 8, 9, 10, 11, 3, 12, 8, 13, 14, 7, 15]
+
 def get_number_with_highest_count(counts):  # <1>
-    max_count = 0
-    for number, count in counts.items():
-        if count > max_count:
-            max_count = count
-            number_with_highest_count = number
-    return number_with_highest_count
+    return max(
+        counts,
+        key=lambda number: counts[number]
+    )
 
 
 def most_frequent(numbers):
-    counts = {}
-    for number in numbers:  # <2>
-        if number in counts:
-            counts[number] += 1
-        else:
-            counts[number] = 1
+    counts = Counter(numbers)
 
     return get_number_with_highest_count(counts)
+
+print(most_frequent(num_list))
