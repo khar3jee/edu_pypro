73f78e6d5d202491a9ff3321f7c5e5e5011c1b56
Dave Cardenas <khar3jee@pm.me>
2022-09-04 20:22:56 -0700

    Add counter template with the two functions

---
+++ number_fun.py
@@ -0,0 +1,24 @@
+"""
+Numbers and counting
+Change the functions into more efficient ones
+(write up what I learned with functions from collections)
+"""
+
+def get_number_with_highest_count(counts):  # <1>
+    max_count = 0
+    for number, count in counts.items():
+        if count > max_count:
+            max_count = count
+            number_with_highest_count = number
+    return number_with_highest_count
+
+
+def most_frequent(numbers):
+    counts = {}
+    for number in numbers:  # <2>
+        if number in counts:
+            counts[number] += 1
+        else:
+            counts[number] = 1
+
+    return get_number_with_highest_count(counts)
